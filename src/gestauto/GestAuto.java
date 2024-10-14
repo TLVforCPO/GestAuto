3476f90ddb5886c0871c1260c63ae14b20ed2134
thoma <thoma@10.1.60.13>
2024-10-14 10:16:19 +0200

    Création du dépot

---
+++ src/gestauto/GestAuto.java
@@ -0,0 +1,16 @@
+package gestauto;
+
+/**
+ *
+ * @author thoma
+ */
+public class GestAuto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
