d96c259237a521d41cf7b0b5f81ae12eacc4c018
thoma <thoma@10.1.60.13>
2024-10-16 14:24:13 +0200

    Création du dépot

---
+++ src/Classes/Client.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author thoma
+ */
+public class Client {
+    private int ID_client;
+    
+}
--- src/Classes/GestAuto.java
+++ src/Classes/GestAuto.java
@@ -1,4 +1,4 @@
-package gestauto;
+package Classes;
 
 /**
  *
